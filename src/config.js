3950361d40765ca52f9aabd58f8661f7397c1f69
Diego Diaz <berith.py@gmail.com>
2018-01-27 17:15:48 -0300

    Some combos from sotn and street fighter.

--- src/config.js
+++ src/config.js
@@ -26,4 +26,19 @@ config.T={
 config.horizontalTiles = config.gameWidth / config.tileWidth;
 config.verticalTiles = config.gameHeight / config.tileHeight;
 
+config.signals=[
+  {pattern:[U,U,D,D,L,R,L,R,M,M],difficulty:0,},
+  {pattern:[L,U,R,M],difficulty:0},
+  {pattern:[L,R,U,D,M],difficulty:0},
+  {pattern:[U,D,R,M],difficulty:0},
+  {pattern:[U,R,D,M],difficulty:0},
+  {pattern:[L,R,D,L,R,M],difficulty:0},
+  {pattern:[D,R,M],difficulty:0},
+  {pattern:[M,U,L,D,R,M],difficulty:0},
+  {pattern:[D,R,U,D,M],difficulty:0},
+  {pattern:[M,R,D,R,D],difficulty:0},
+  {pattern:[M,D,L,U,M],difficulty:0},
+  {pattern:[L,D,R,M],difficulty:0},
+  {pattern:[D,R,M],difficulty:0},
+  ]
 export default config
