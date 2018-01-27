30b27b81663750cfb18fd329636fb7c615388a5f
Alejo Carballude <alejocarballude@gmail.com>
2018-01-27 15:08:00 -0300

    Config includes tile size and amount of tiles.

--- src/config.js
+++ src/config.js
@@ -1,5 +1,12 @@
-export default {
+config = {
   gameWidth: 960,
   gameHeight: 544,
+  tileWidth: 16,
+  tileHeight: 16,
   localStorageName: 'transmissionADKL'
 }
+
+config.horizontalTiles = config.gameWidth / config.tileWidth;
+config.verticalTiles = config.gameHeight / config.tileHeight;
+
+export default config
