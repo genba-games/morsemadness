459e596d81cad0cb5c9b4ea894f10ad9d3568be8
Alejo Carballude <alejocarballude@gmail.com>
2018-01-27 03:04:54 -0300

    Moved maze into its own module. Generates doors.

--- src/states/Game.js
+++ src/states/Game.js
@@ -1,58 +1,9 @@
 /* globals __DEV__ */
 import Phaser from 'phaser'
 import Mushroom from '../sprites/Mushroom'
-import generate from 'generate-maze'
 import dweller from '../sprites/dweller'
+import generateMaze from '../code/maze'
 
-var TILETYPE = {
-  CLEAR: 0,
-  WALL: 1,
-  DOOR_LR: 2,
-  DOOR_UD: 3,
-};
-
-/**
- * Generates a maze and populates it doors and objects.
- * @param {Number} height Height of the maze. MUST be odd.
- * @param {Number} width Width of the maze. MUST be odd.
- * @returns {Array[Array]} Matrix of the specified width and height containing 
- * TILETYPE entries.
- */
-function generateMaze(width, height) {
-  if (width % 2 == 0 || height % 2 == 0)
-    throw 'Cannot generate maze with even dimensions. Dimensions MUST be odd!';
-
-  // Reduce the space for the generator
-  let gen = generate((width + 1) / 2, (height + 1) / 2);
-
-  console.log(gen)
-
-  let maze = [];
-  for (let x=0; x < height; x++)
-    maze.push(Array(width));
-
-  for (let x=0; x < height; x += 2) {
-    for (let y=0; y < width; y += 2) {
-      let piece = gen[x/2][y/2];
-      console.log('xy', x, y, 'lrud', piece.left, piece.right, piece.top, piece.bottom);
-      // Set the current position to CLEAR
-      maze[x][y] = TILETYPE.CLEAR;
-      // Set the corner down right to wall
-      if (x < height - 1 && y < width - 1)
-        maze[x+1][y+1] = TILETYPE.WALL;
-      // Set the right and bottom walls
-      if (x < height - 1)
-        maze[x+1][y] = piece.bottom ? TILETYPE.WALL : TILETYPE.CLEAR;
-      if (y < width - 1)
-        maze[x][y+1] = piece.right ? TILETYPE.WALL : TILETYPE.CLEAR;
-    }
-    console.log(' ');
-  }
-
-  for (let x=0; x < height; x++)
-    console.log(maze[x]);
-
-};
 
 export default class extends Phaser.State {
   init () {}
