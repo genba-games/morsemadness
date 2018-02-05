866363a140172908b49fc44c0ff47ab3331962f5
Alejo Carballude <alejocarballude@gmail.com>
2018-02-04 23:50:25 -0300

    Formatted Gamepad file.

--- src/code/gamepad/gamepad.js
+++ src/code/gamepad/gamepad.js
@@ -87,7 +87,7 @@ var defaultKeymap = [{
  */
 function keyPressed(keymap, key) {
     if (keymap.buttons) {
-        let buttons = keymap.buttons
+        let buttons = keymap.buttons;
         key = buttons[key];
         // Check if key was processed by Phaser
         if (key === true || key === false)
@@ -103,27 +103,27 @@ function keyPressed(keymap, key) {
     }
 }
 
-
 function axisPressed(keymap, axis, direction) {
     if (keymap.axes) {
-        let axes = keymap.axes
-        axis = axes[axis]
+        let axes = keymap.axes;
+        axis = axes[axis];
         for (let a in axis) {
-            a = axis[a]
+            a = axis[a];
             if (direction === '+') {
                 if (this.pad && this.pad.axis(a) < -0.8) {
-                    return true
+                    return true;
                 }
             } else {
                 if (this.pad && this.pad.axis(a) > 0.8) {
-                    return true
+                    return true;
                 }
             }
         }
-        return false
+        return false;
     }
 
 }
+
 /**
  * Creates a new gamepad with the specified keys for a player.
  * 
@@ -135,40 +135,40 @@ function axisPressed(keymap, axis, direction) {
  */
 function Gamepad(player, keymap, pad) {
     this.player = player;
-    this.createGamepad(pad)
-    this.keymap=KEYMAPS[pad]
-    this._keyPressed = keyPressed
+    this.createGamepad(pad);
+    this.keymap = KEYMAPS[pad];
+    this._keyPressed = keyPressed;
     this.keyPressed = function (key) {
         return this._keyPressed(this.keymap, key);
-    }
-    this._axisPressed = axisPressed
+    };
+    this._axisPressed = axisPressed;
     this.axisPressed = function (axis, direction) {
         return this._axisPressed(this.keymap, axis, direction);
-    }
+    };
 };
+
 Gamepad.prototype.createGamepad = function (pad) {
-    this.pad = this.player.game.input.gamepad[pad]
-    console.log(pad)
-    console.log(this.pad)
+    this.pad = this.player.game.input.gamepad[pad];
     this.pad.addCallbacks(this, {
         onConnect: this.setKeymap
-    })
+    });
 }
 
 Gamepad.prototype.setKeymap = function () {
-    //This callback function executes when a pad is connected,
-    //it looks up into our KEYMAPS and if it doesn't find it, it sets the default ps3/xbox360 gamepad
-    let id = this.pad._rawPad.id
-    let padKeymap
+    // This callback function executes when a pad is connected,
+    // it looks up into our KEYMAPS and if it doesn't find it, 
+    // it sets the default ps3/xbox360 gamepad
+    let id = this.pad._rawPad.id;
+    let padKeymap = {};
     for (let keymap in KEYMAPS) {
         keymap = KEYMAPS[keymap]
         if (keymap.id == id) {
-            padKeymap = keymap
+            padKeymap = keymap;
         }
     }
-    merge(this.keymap,padKeymap)
-
+    merge(this.keymap, padKeymap);
 }
+
 module.exports = {
     GAMEPAD_KEY: GAMEPAD_KEY,
     Gamepad: Gamepad,
